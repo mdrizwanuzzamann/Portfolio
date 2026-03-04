af3b823c8281cc28264dc0ad09ceb8d303d280fa
mdrizwanuzzmann <mdrizwanuzzaman46@gmail.com>
2026-03-04 14:48:54 +0530

    changes done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
       <div className="md:w-2/4 md:pt-10 mt-16">
         <h1 className="text-xl md:text-6xl font-bold flex leading-normal tracking-tighter">
-        Hi, I'm Rizwan
+          Hi, I'm Rizwan
         </h1>
         <p className="text-sm mb-8 md:text-2xl tracking-tight ">
           A Full Stack Web Developer & Tech Enthusiast
@@ -18,15 +18,14 @@ const Home = () => {
         </a>
 
         {/* Put resume link below 👇 */}
-        <a href="https://drive.google.com/file/d/1evO978nu6FFr_hNwFfduXtpSsl9yRlhh/view?usp=sharing" >
+        <a href="https://drive.google.com/file/d/1d23Q2Kv2uB7l4UXb-XZ4Nap6I-W_nc9c/view?usp=sharing">
           <button className="cursor-pointer mx-2 mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
             Download Resume
           </button>
         </a>
       </div>
       <div>
-                <img className="" src={avatarImg} alt="" />
-        
+        <img className="" src={avatarImg} alt="" />
       </div>
     </div>
   );
