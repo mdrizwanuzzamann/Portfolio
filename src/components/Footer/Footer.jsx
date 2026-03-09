193a0d60445b751c6ee785b49069c8301826f26d
mdrizwanuzzamann <mdrizwanuzzaman46@gmail.com>
2026-03-10 03:19:16 +0530

    Footer modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,26 +7,25 @@ const Footer = () => {
   return (
     <div
       id="Footer"
-      className="flex justify-around bg-[#465697] text-white p-10 md:p-12 items-center "
+      className="flex flex-col sm:flex-row justify-around bg-[#465697] text-white p-8 sm:p-10 md:p-12 items-center gap-6 sm:gap-0"
     >
-      <div>
-        <h1 className="text-2xl md:text-6xl font-bold">Contact</h1>
-        <h3 className="text-2xl md:text-2xl font-normal">Feel Free to reach out!</h3>
+      <div className="text-center sm:text-left">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold">Contact</h1>
+        <h3 className="text-base sm:text-lg md:text-2xl font-normal">Feel Free to reach out!</h3>
       </div>
 
-      <ul className="text-sm md:text-xl">
-        <li className="flex gap-1 items-center">
+      <ul className="text-sm sm:text-base md:text-xl flex flex-col gap-3">
+        <li className="flex gap-2 items-center justify-center sm:justify-start">
           <MdOutlineEmail size={20} />
-          <a href="mailto:mdrizwanuzzaman46@gmail.com">Email</a>
+          <a href="mailto:mdrizwanuzzaman46@gmail.com" className="hover:underline">Email</a>
         </li>
-        <li className="flex gap-1 items-center">
-          <CiLinkedin />
-          <a href="https://www.linkedin.com/in/mdrizwanuzzaman/">LinkedIn</a>
+        <li className="flex gap-2 items-center justify-center sm:justify-start">
+          <CiLinkedin size={20} />
+          <a href="https://www.linkedin.com/in/mdrizwanuzzaman/" className="hover:underline">LinkedIn</a>
         </li>
-
-        <li className="flex gap-1 items-center">
-          <FaGithub />
-          <a href="https://github.com/mdrizwanuzzamann">GitHub</a>
+        <li className="flex gap-2 items-center justify-center sm:justify-start">
+          <FaGithub size={20} />
+          <a href="https://github.com/mdrizwanuzzamann" className="hover:underline">GitHub</a>
         </li>
       </ul>
     </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-  return <div id="Projects" className="p-10px md:p-24 text-white">
-    <h1 className="text-2xl md:text-4xl text-white font-bold">
+  return (
+    <div id="Projects" className="px-6 py-10 sm:px-10 sm:py-14 md:px-24 md:py-20 text-white">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl text-white font-bold mb-2">
         Projects
-        <div className="py-12 px-8 flex flex-wrap gap-5">
-            <ProjectCard title="Blogging Website" main="This is a blogging website created in Next.js and used some component library" /> 
-            <ProjectCard title="Youtube Clone" main="This is a blogging website created in Next.js and used some component library" /> 
-            <ProjectCard title="Netflix Clone" main="This is a blogging website created in Next.js and used some component library" /> 
+      </h1>
 
-
-
-        </div>
-    </h1>
-
-  </div>;
+      <div className="py-8 flex flex-wrap justify-center sm:justify-start gap-6">
+        <ProjectCard title="Blogging Website" main="This is a blogging website created in Next.js and used some component library" />
+        <ProjectCard title="Youtube Clone" main="This is a blogging website created in Next.js and used some component library" />
+        <ProjectCard title="Netflix Clone" main="This is a blogging website created in Next.js and used some component library" />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
